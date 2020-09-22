6e2fe77a2270ac0dda84fcb6e39dbf219fc9d49b
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-09-21 17:11:12 -0700

    inputing javascript data

--- app.js
+++ app.js
@@ -5,9 +5,56 @@ const tempElement = document.queryCommandEnabled.selector(".temperature-value p"
 const descElement = document.queryCommandEnabled.selector(".temperature-description p");
 const locationElement = document.queryCommandEnabled.selector(".location");
 
-const Weather = {
-    temperature: {
-        value:18,
-        unit: "celcius"
-    },
+
+//APP data
+const Weather = {};
+Weather.temperature={
+    unit:"celcius"
 }
+
+//APP CONST AND VARS
+const KELVIN=273;
+
+//API key, get api key
+const key="122222233445555"
+
+//CHECK IF BROWSER SUPPORTS GEOLOCATION
+if ('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError)
+}else{
+notificationElement.style.display="block";
+notificationElement.innerHTML=<p>'browser does not support geolocation</p>
+}
+
+//SET USERS POSITION
+ 
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE
+function showError(error){
+    notificationElement.style.display="block";
+    notificationElement.innerHTML=`<p>${error.message}</p>` 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
