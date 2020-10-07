424b7319bdc47fba5dda0e52656bf308b62fcb0a
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-10-07 14:16:22 -0700

    completed js sextion

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-//select elements
-const notificationElement = document.getElementById(".notification");
-const iconElement =  document.getElementById("weather-icon");
-const tempElement =  document.getElementById("temperature-value p");
-const descElement = document.getElementById("temperature-description p");
-const locationElement =  document.getElementById("location");
 
 
-//APP data
-const Weather = {};
-Weather.temperature={
-    unit:"celcius"
+// SELECT ELEMENTS
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// App data
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
 }
 
-//APP CONST AND VARS
-const KELVIN=273;
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key="98ba8c90620aa1305ffa795fe53cf27d"
 
-//API key, get api key
-const key="eb899420b4msh54a3ba2026f0a44p1d6056jsn3918f241e814"
-
-//CHECK IF BROWSER SUPPORTS GEOLOCATION
-if ('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError)
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
 }else{
-
-notificationElement.innerHTML=`<p>'browser does not support geolocation</p>`
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
 
-//SET USERS POSITION
- 
+// SET USER'S POSITION
 function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
@@ -35,74 +35,58 @@ function setPosition(position){
     getWeather(latitude, longitude);
 }
 
-// SHOW ERROR WHEN THERE IS AN ISSUE
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
 function showError(error){
-    notificationElement.innerHTML=`<p>${error.message}</p>` 
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
-//GET WEATHER FROM API PROVIDER
-function  getWeather(latitude, longitude){
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     
-    `lat=${latitude}&lon=${longitude}&appid=${key}`
-
-    fetch("https://rapidapi.p.rapidapi.com/weather?q=Lagos%2CNigeria&callback=test&id=2172797&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-            "x-rapidapi-key": "bPnHUyGLA9msh2loEskcGkXU20bep1Z18jwjsnSxybvwmVlN8L"
-        }
-    })
-    .then(function(response){
-        console.log(response)
-     let data = response.json();
-     return data ;  
-    })
-    .then(function(data){
-        Weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-        Weather.description = data.weather[0].description;
-        Weather.iconId=data.weather[0].icon;
-        Weather.city= data.name;
-        Weather.country = data.sys.country
-
-    })
-    .then(function(){
-        displayWeather()
-    });
-
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
 }
-  //DISPLAY WEATHER TO UI
-  function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${Weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${Weather.temperature.value}° <span>C</span>`;
-    descElement.innerHTML = Weather.description;
-   locationElement.innerHTML = `${Weather.city}, ${Weather.country}`;
-   }
-   //CELCIUS TO FAHRENHEIT CONVERSION
-   function celciusToFahrenheit(temperature){
-      return (temperature * 9/5) + 32 
-   }
 
-   // when the user clicks on the temperature
-   tempElement.addEventListener("click", function(){
-       if (weather.temperature.value === undefined){return}
-       if(weather.temperature.value == celcius){
-         let fahrenheit = celciusToFahrenheit(weather.temperature.value);
-         fahrenheit = Math.floor(fahrenheit);
-         tempElement.innerHTML = `${fahrenheit}° <span>F</span>`;
-         weather.temperature.unit = "fahrenheit"
-       }else{
-        tempElement.innerHTML = `${Weather.temperature.value}° <span>C</span>`;
-        weather.temperature.unit = "celcius"
-       }
-   })
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
 
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
 
-
-
-
-
-  
-
-
-
-
-  
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
